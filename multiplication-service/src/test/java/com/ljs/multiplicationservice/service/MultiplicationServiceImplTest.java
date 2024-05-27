943bfd79f595976032490428ad4146eb6d065e94
LEE JINSEOP <jjjp9966@gmail.com>
2024-05-27 17:26:31 +0900

    test: Add MultiplicationServiceImpl test cases
    - Add test case to check correct and wrong multiplication attempts

--- multiplication-service/src/test/java/com/ljs/multiplicationservice/service/MultiplicationServiceImplTest.java
+++ multiplication-service/src/test/java/com/ljs/multiplicationservice/service/MultiplicationServiceImplTest.java
@@ -1,25 +1,35 @@
 package com.ljs.multiplicationservice.service;
 
+import com.ljs.multiplicationservice.dto.MultiplicationAttemptRequest;
 import com.ljs.multiplicationservice.dto.MultiplicationDto;
+import com.ljs.multiplicationservice.entity.MultiplicationAttempt;
+import com.ljs.multiplicationservice.repository.MultiplicationAttemptRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
 
 public class MultiplicationServiceImplTest {
     private MultiplicationService multiplicationService;
 
     @Mock
+    private MultiplicationAttemptRepository multiplicationAttemptRepository;
+
+    @Mock
     private RandomGeneratorService randomGeneratorService;
+
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        multiplicationService = new MultiplicationServiceImpl(randomGeneratorService);
+        multiplicationService = new MultiplicationServiceImpl(multiplicationAttemptRepository, randomGeneratorService);
     }
 
     @Test
@@ -34,5 +44,33 @@ public class MultiplicationServiceImplTest {
         // then
         assertEquals(50, multiplicationDto.getFactorA());
         assertEquals(30, multiplicationDto.getFactorB());
+    }
+
+    @Test
+    @DisplayName("무작위 곱셉 정답 테스트")
+    public void checkCorrectAnswerTest() {
+        // given
+        MultiplicationAttemptRequest request = new MultiplicationAttemptRequest("ljs", new MultiplicationDto(10, 20), 200);
+
+        // when
+        boolean result = multiplicationService.checkAnswer(request);
+
+        // then
+        assertTrue(result);
+        verify(multiplicationAttemptRepository).save(any(MultiplicationAttempt.class));
+    }
+
+    @Test
+    @DisplayName("무작위 곱셈 오답 테스트")
+    public void checkWrongAnswerTest() {
+        // given
+        MultiplicationAttemptRequest request = new MultiplicationAttemptRequest("ljs", new MultiplicationDto(10, 20), 20);
+
+        // when
+        boolean result = multiplicationService.checkAnswer(request);
+
+        // then
+        assertFalse(result);
+        verify(multiplicationAttemptRepository).save(any(MultiplicationAttempt.class));
     }
 }
