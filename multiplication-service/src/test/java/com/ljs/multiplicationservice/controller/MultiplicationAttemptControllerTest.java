9a90509c27d89a76ec52a60d4e5ad107d1cfc3eb
LEE JINSEOP <jjjp9966@gmail.com>
2024-05-27 17:31:19 +0900

    test: Add MultiplicationAttemptController test case
    - Add test case for API to check multiplication answer

---
+++ multiplication-service/src/test/java/com/ljs/multiplicationservice/controller/MultiplicationAttemptControllerTest.java
@@ -0,0 +1,69 @@
+package com.ljs.multiplicationservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ljs.multiplicationservice.dto.MultiplicationAttemptRequest;
+import com.ljs.multiplicationservice.dto.MultiplicationAttemptResponse;
+import com.ljs.multiplicationservice.dto.MultiplicationDto;
+import com.ljs.multiplicationservice.service.MultiplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(MultiplicationAttemptController.class)
+class MultiplicationAttemptControllerTest {
+    @MockBean
+    private MultiplicationService multiplicationService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private JacksonTester<MultiplicationAttemptRequest> jsonRequest;
+    private JacksonTester<MultiplicationAttemptResponse> jsonResponse;
+
+    @BeforeEach
+    public void setUp() {
+        JacksonTester.initFields(this, new ObjectMapper());
+    }
+
+    @Test
+    @DisplayName("무작위 곱셈 정답 테스트")
+    public void postAnswerReturnCorrect() throws Exception {
+        genericParameterizedTest(true);
+    }
+
+    @Test
+    @DisplayName("무작위 곱셈 실패 테스트")
+    public void postAnswerReturnNotCorrect() throws Exception {
+        genericParameterizedTest(false);
+    }
+
+    private void genericParameterizedTest(boolean correct) throws Exception {
+        // given
+        MultiplicationAttemptRequest multiplicationAttemptRequest = new MultiplicationAttemptRequest("ljs", new MultiplicationDto(10, 20), 200);
+        MultiplicationAttemptResponse multiplicationAttemptResponse = new MultiplicationAttemptResponse("ljs", new MultiplicationDto(10, 20), 200, correct);
+        given(multiplicationService.checkAnswer(any(MultiplicationAttemptRequest.class))).willReturn(correct);
+
+        // when
+        MockHttpServletResponse response = mvc.perform(post("/api/multiplication/attempt")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest.write(multiplicationAttemptRequest).getJson()))
+                .andReturn().getResponse();
+
+        // then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(jsonResponse.write(multiplicationAttemptResponse).getJson(), response.getContentAsString());
+    }
+}
