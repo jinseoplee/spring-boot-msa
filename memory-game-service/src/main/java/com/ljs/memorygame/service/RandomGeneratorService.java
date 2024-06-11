f20d33d33985f014ea5e848a5eda9c7c6cf15592
LEE JINSEOP <jjjp9966@gmail.com>
2024-06-11 21:19:36 +0900

    feat: Add random number generating function

---
+++ memory-game-service/src/main/java/com/ljs/memorygame/service/RandomGeneratorService.java
@@ -0,0 +1,10 @@
+package com.ljs.memorygame.service;
+
+public interface RandomGeneratorService {
+    /**
+     * 임의의 정수를 생성한다.
+     *
+     * @return 랜덤으로 생성된 정수
+     */
+    int generateNumber();
+}
---
+++ memory-game-service/src/main/java/com/ljs/memorygame/service/RandomGeneratorServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ljs.memorygame.service;
+
+import java.util.Random;
+
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+    public static final int MINIMUM = 1;
+    public static final int MAXIMUM = 99;
+
+    /**
+     * @return 1 ~ 99 사이의 랜덤으로 생성된 정수
+     */
+    @Override
+    public int generateNumber() {
+        return new Random().nextInt(MAXIMUM - MINIMUM + 1) + MINIMUM;
+    }
+}
